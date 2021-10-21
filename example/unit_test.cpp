cee2bae028f70f190a052b6e2c639b5366194d8f
syw2014 <jerryshi0110@gmail.com>
2021-10-21 18:36:39 +0800

    fix errors with namespace

--- example/unit_test.cpp
+++ example/unit_test.cpp
@@ -1,71 +1,66 @@
 /*
  * @Author: your name
  * @Date: 2021-10-14 10:37:16
- * @LastEditTime: 2021-10-14 14:07:36
+ * @LastEditTime: 2021-10-21 18:26:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dialogue-service\src\unit_test.cpp
  */
-/*************************************************************************
-# File Name: entity_extract.cc
-# Method: 
-# Author: Jerry Shi
-# Mail: jerryshi0110@gmail.com
-# Created Time: 2020年07月16日  10:58:54
- ************************************************************************/
 // #include "entity_extract.h"
 // #include "slot_entity_extract.h"
 // #include "inverted_index.h"
 #include "ahocorasick.h"
 #include <time.h>
 
-
 void usage() {
-	printf("Usage:\n");
-	printf("\t ./exec-file -dict_dir [input dictionary path] -input [input document filename] \
+    printf("Usage:\n");
+    printf(
+        "\t ./exec-file -dict_dir [input dictionary path] -input [input document filename] \
             -output [output match result filename]\n");
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
+    // TEST ahc keyword search for NER
+    std::vector<std::string> keywords = {
+        "王者荣耀", "王者",   "天空之城", "稻花香", "哈利波特",
+        "悟",       "我想要", "帮我",     "你好",   "您好"};
+    std::vector<int> labels = {1, 1, 1, 2, 3, 4, 4, 0, 0, 0};
+    StringMatching::ACAutomaton *ahc = new StringMatching::ACAutomaton();
 
-	// TEST ahc keyword search for NER
-	std::vector<std::string> keywords = {"王者荣耀","王者","天空之城","稻花香","哈利波特","悟","我想要","帮我","你好","您好"};
-	std::vector<int> labels = {1, 1, 1, 2, 3, 4, 4, 0, 0, 0};
-	ACAutomaton *ahc = new ACAutomaton();
-	
-	// add keywords to ahc
-	for (int i = 0; i < keywords.size(); ++i) {
-		ahc->AddWord(keywords[i], labels[i]);
-	}
+    // add keywords to ahc
+    for (int i = 0; i < keywords.size(); ++i) {
+        ahc->AddWord(keywords[i], labels[i]);
+    }
 
-	// build automation
-	ahc->Build();
+    // build automation
+    ahc->Build();
 
-	// test
-	std::map<std::string, TrieNode*> nodes;
+    // test
+    std::map<std::string, StringMatching::TrieNode *> nodes;
 
-	std::vector<std::string> tstr1 = {"打开王者de王者荣耀", "打开王者荣耀", "打开王者", "帮我找下王者荣耀", "你好，王者","您好，我想要听稻花香", "帮我找含有悟的专辑，谢谢"};
+    std::vector<std::string> tstr1 = {
+        "打开王者de王者荣耀",      "打开王者荣耀", "打开王者",
+        "帮我找下王者荣耀",        "你好，王者",   "您好，我想要听稻花香",
+        "帮我找含有悟的专辑，谢谢"};
 
-	for (int i = 0; i < tstr1.size(); ++i) {
-		nodes.clear();
-		// ahc->Search(tstr1[i], nodes);
-		ahc->SearchLongest(tstr1[i], nodes);
+    for (int i = 0; i < tstr1.size(); ++i) {
+        nodes.clear();
+        // ahc->Search(tstr1[i], nodes);
+        ahc->SearchLongest(tstr1[i], nodes);
 
-		// print search results
-		std::cout << "Input sentence: " << tstr1[i] << std::endl;
-			// parse search results
-		for (std::map<std::string, TrieNode*>::iterator iter = nodes.begin(); iter != nodes.end(); ++iter) {
-			std::cout << "Keywords: " << iter->second->word << "\tindex: " << iter->second->index <<
-				"\tlabel id: " << iter->second->label << std::endl;
-		}
-		std::cout << "\n-------------------------------------------\n";
-	}
+        // print search results
+        std::cout << "Input sentence: " << tstr1[i] << std::endl;
+        // parse search results
+        for (std::map<std::string, StringMatching::TrieNode *>::iterator iter =
+                 nodes.begin();
+             iter != nodes.end(); ++iter) {
+            std::cout << "Keywords: " << iter->second->word
+                      << "\tindex: " << iter->second->index
+                      << "\tlabel id: " << iter->second->label << std::endl;
+        }
+        std::cout << "\n-------------------------------------------\n";
+    }
 
-
-
-	return 0;
+    return 0;
 }
-
-
-
--- src/common/common.cc
+++ src/common/common.cc
@@ -1,26 +1,26 @@
 /*
  * @Author: yw.shi
  * @Date: 2020-07-16 11:17:14
- * @LastEditTime: 2020-11-17 10:01:26
+ * @LastEditTime: 2021-10-21 18:28:41
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: usefull tools for data process
  */
- 
+
 #include "common.h"
 
 // Get file list from given path
 void GetFilenames(std::string path, std::vector<std::string> &files) {
     DIR *pDir;
-    struct dirent* ptr;
-    if(!(pDir= opendir(path.c_str()))) {
+    struct dirent *ptr;
+    if (!(pDir = opendir(path.c_str()))) {
         perror(("Folder " + path + "doesn't exist!").c_str());
         return;
     }
-    while((ptr = readdir(pDir)) != 0) {
-        if(strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0) {
+    while ((ptr = readdir(pDir)) != 0) {
+        if (strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0) {
             // std::cout << ptr->d_name << std::endl;
-            files.push_back(path+"/"+ptr->d_name);
+            files.push_back(path + "/" + ptr->d_name);
         }
     }
     closedir(pDir);
@@ -29,13 +29,13 @@ void GetFilenames(std::string path, std::vector<std::string> &files) {
 // Only get the file name in the given path without path
 void GetFileWithoutPath(std::string path, std::vector<std::string> &files) {
     DIR *pDir;
-    struct dirent* ptr;
-    if(!(pDir= opendir(path.c_str()))) {
+    struct dirent *ptr;
+    if (!(pDir = opendir(path.c_str()))) {
         perror(("Folder " + path + " doesn't exist!").c_str());
         return;
     }
-    while((ptr = readdir(pDir)) != 0) {
-        if(strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0) {
+    while ((ptr = readdir(pDir)) != 0) {
+        if (strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0) {
             // std::cout << ptr->d_name << std::endl;
             files.push_back(ptr->d_name);
         }
@@ -43,41 +43,42 @@ void GetFileWithoutPath(std::string path, std::vector<std::string> &files) {
     closedir(pDir);
 }
 
-
 // String split with specific pattern
-void SplitWord(const std::string &str, std::vector<std::string>& vec, const std::string& pattern) {
+void SplitWord(const std::string &str, std::vector<std::string> &vec,
+               const std::string &pattern) {
     std::string::size_type pos1, pos2;
-	pos1 = 0;
-	pos2 = str.find(pattern);
-	while (std::string::npos != pos2) {
-		vec.push_back(str.substr(pos1, pos2 - pos1));
-		pos1 = pos2 + pattern.size();
-		pos2 = str.find(pattern, pos1);
-	}
-	if (pos1 != str.length()) {
-		vec.push_back(str.substr(pos1));
-	}
+    pos1 = 0;
+    pos2 = str.find(pattern);
+    while (std::string::npos != pos2) {
+        vec.push_back(str.substr(pos1, pos2 - pos1));
+        pos1 = pos2 + pattern.size();
+        pos2 = str.find(pattern, pos1);
+    }
+    if (pos1 != str.length()) {
+        vec.push_back(str.substr(pos1));
+    }
 }
 
 // Parse arguments given from commond line
 int ArgumentParse(char *str, int argc, char **argv) {
     int a;
-	for (a = 1; a < argc; ++a) if (!strcmp(str, argv[a])) {
-		if (a == argc - 1) {
-			printf("Argument missing for %s\n", str);
-			exit(1);
-		}
-		return a;
-	}
-	return -1;
+    for (a = 1; a < argc; ++a)
+        if (!strcmp(str, argv[a])) {
+            if (a == argc - 1) {
+                printf("Argument missing for %s\n", str);
+                exit(1);
+            }
+            return a;
+        }
+    return -1;
 }
 
-wchar_t* MBCS2Unicode(wchar_t *buff, const char *str) {
-    wchar_t* wp = buff;
-    char* p = (char*)str;
-    while(*p) {
-        if(*p & 0x80) {
-            *wp = *(wchar_t*)p;
+wchar_t *MBCS2Unicode(wchar_t *buff, const char *str) {
+    wchar_t *wp = buff;
+    char *p = (char *)str;
+    while (*p) {
+        if (*p & 0x80) {
+            *wp = *(wchar_t *)p;
             p++;
         } else {
             *wp = (wchar_t)*p;
@@ -89,16 +90,16 @@ wchar_t* MBCS2Unicode(wchar_t *buff, const char *str) {
     return buff;
 }
 
-char* Unicode2MBCS(char *buff, const wchar_t *str) {
+char *Unicode2MBCS(char *buff, const wchar_t *str) {
     wchar_t *wp = (wchar_t *)str;
     char *p = buff, *tmp;
-    while(*wp) {
+    while (*wp) {
         tmp = (char *)wp;
-        if(*wp & 0xFF00) {
+        if (*wp & 0xFF00) {
             *p = *tmp;
             p++;
             tmp++;
-            *p= *tmp;
+            *p = *tmp;
             p++;
         } else {
             *p = *tmp;
@@ -110,11 +111,10 @@ char* Unicode2MBCS(char *buff, const wchar_t *str) {
     return buff;
 }
 
-
 // Convert string to wstring based on wchar_t and char
-std::wstring Str2Wstr(std::string const & src){
+std::wstring Str2Wstr(std::string const &src) {
     std::wstring dest;
-    //std::setlocale(LC_CTYPE, "");
+    // std::setlocale(LC_CTYPE, "");
     std::setlocale(LC_CTYPE, "en_US.utf8");
     size_t const wcs_len = mbstowcs(NULL, src.c_str(), 0);
     std::vector<wchar_t> tmp(wcs_len + 1);
@@ -124,7 +124,7 @@ std::wstring Str2Wstr(std::string const & src){
 }
 
 // Convert Wstring to string
-std::string Wstr2Str(std::wstring const & src){
+std::string Wstr2Str(std::wstring const &src) {
     std::string dest;
     std::setlocale(LC_CTYPE, "");
     size_t const mbs_len = wcstombs(NULL, src.c_str(), 0);
