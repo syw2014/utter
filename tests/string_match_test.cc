0b4b87941ea953fc851a56827cff755c0aa8f7df
syw2014 <jerryshi0110@gmail.com>
2021-10-27 14:35:29 +0800

    add more unit test for string match module

--- tests/string_match_test.cc
+++ tests/string_match_test.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-20 14:52:29
- * @LastEditTime: 2021-10-27 10:21:52
+ * @LastEditTime: 2021-10-27 14:32:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dialogue-service\tests\temp_engine_test.cc
@@ -24,7 +24,7 @@ std::vector<std::string> keywords = {
     "王者荣耀", "王者",     "天空之城",     "稻花香",      "哈利波特",
     "悟",       "我想要",   "帮我",         "你好",        "您好",
     "ShangHai", "London",   "London Tower", "13578921465", "万夫2",
-    "iphone",   "iphone13", "iphone13 pro"};
+    "iphone",   "iphone13", "iphone13pro"};
 // Test ids as labels of keywords
 std::vector<int> labels = {1, 1, 1, 2, 3,  4,  4,  0,  0,
                            0, 5, 5, 5, 10, 11, 12, 12, 12};
@@ -79,6 +79,7 @@ TEST(StringMatchUnitTest, DigitalNotMatch) {
 
 // Test5, all keywords match
 TEST(StringMatchUnitTest, AllSearch) {
+    nodes.clear();
     std::vector<std::string> expected = {"王者", "王者荣耀", "万夫2"};
     std::string sentence = "打开王者荣耀，万夫2.0";
     ahc->Search(sentence, nodes);
@@ -88,11 +89,24 @@ TEST(StringMatchUnitTest, AllSearch) {
     EXPECT_THAT(entity_words, ::testing::UnorderedElementsAreArray(expected));
 }
 
-// Test6, longest keywords match
+// Test6, all keywords match
+TEST(StringMatchUnitTest, AllCombinationSearch) {
+    nodes.clear();
+    std::vector<std::string> expected = {"IPHONE", "IPHONE13", "IPHONE13PRO"};
+    std::string sentence = "我想买iphone13pro max手机";
+    ahc->Search(sentence, nodes);
+    ahc->ParseSearchWords(nodes, entity_words, index, label_ids);
+
+    EXPECT_EQ(expected.size(), entity_words.size());
+    EXPECT_THAT(entity_words, ::testing::UnorderedElementsAreArray(expected));
+}
+
+// Test7, longest keywords match
 TEST(StringMatchUnitTest, LongestSearch) {
+    nodes.clear();
     std::vector<std::string> expected = {"王者", "王者荣耀", "万夫2"};
     std::string sentence = "打开王者王者荣耀，万夫2.0";
-    ahc->Search(sentence, nodes);
+    ahc->SearchLongest(sentence, nodes);
     ahc->ParseSearchWords(nodes, entity_words, index, label_ids);
 
     EXPECT_EQ(expected.size(), entity_words.size());
